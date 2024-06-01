ca73aac0132cd2cff150872cea681bfc12aa4169
ShiftIns <wujunkui407@icloud.com>
2024-06-01 21:59:06 +0800

    develop

--- docs/.vitepress/theme/components/layout/MyLayout.vue
+++ docs/.vitepress/theme/components/layout/MyLayout.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import DefaultTheme from 'vitepress/theme';
+import DefaultTheme from "vitepress/theme"
+import { useData } from "vitepress"
+import Home from "../Home.vue"
 const Layout = DefaultTheme.Layout
 
+const { page, frontmatter } = useData()
 
+const isCustomHome = frontmatter.value.home === true
 </script>
 
 <template>
-<Layout>
-    <template #layout-bottom>
-    </template>
-</Layout>
+  <Home v-if="isCustomHome" />
+  <Layout v-else>
+    <template #layout-bottom> </template>
+  </Layout>
 </template>
