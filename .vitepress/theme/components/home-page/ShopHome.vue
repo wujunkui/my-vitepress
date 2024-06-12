3a3cc1116f1f209a8718ff235531982e0ef9cb90
ShiftIns <wujunkui407@icloud.com>
2024-06-12 22:40:17 +0800

    develop

--- .vitepress/theme/components/home-page/ShopHome.vue
+++ .vitepress/theme/components/home-page/ShopHome.vue
@@ -12,7 +12,6 @@ import ShopItemCard from './components/ShopItemCard.vue'
         <template v-for="{ frontmatter }, index in data" :key="index">
             <div v-if="frontmatter.home !== true" class="flex flex-wrap -m-2">
                 <ShopItemCard :frontmatter="frontmatter" class="m-2" />
-
             </div>
         </template>
     </div>
