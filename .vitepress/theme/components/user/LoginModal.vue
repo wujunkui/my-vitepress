a48b53225ec6f9b29c1414f097178847efe4663d
ShiftIns <wujunkui407@icloud.com>
2024-06-29 20:31:50 +0800

    develop

--- .vitepress/theme/components/user/LoginModal.vue
+++ .vitepress/theme/components/user/LoginModal.vue
@@ -1,16 +1,51 @@
 <script setup lang="ts">
 import { NModal } from 'naive-ui'
 import { useLoginStore } from '../../stores'
-import { toRefs } from 'vue'
 import FormLayout from './components/FormLayout.vue'
+import { NForm, NFormItem, NInput, NButton, useMessage } from 'naive-ui'
+import { DefaultService } from '../../../client'
+import type { LoginUser } from '../../../client'
+import { ref } from 'vue'
 
-const { loginShow, toggleLogin } = toRefs(useLoginStore())
+const loginStore = useLoginStore()
+const message = useMessage()
+const info = ref<LoginUser>({
+    name: '',
+    password: '',
+})
+const submit = () => {
+    DefaultService.login({ requestBody: info.value })
+        .then((res) => {
+            message.success('登录成功')
+            loginStore.toggleLogin()
+        })
+        .catch((err) => {
+            message.error(err.message)
+        })
+}
 </script>
 
 <template>
-    <n-modal v-model:show="loginShow">
+    <n-modal v-model:show="loginStore.loginShow">
         <FormLayout title="登 录" :closeClick="toggleLogin">
 
+            <n-form class="" :model="info">
+                <n-form-item label="用户名">
+                    <n-input v-model:value="info.name" placeholder="用户名"></n-input>
+                </n-form-item>
+                <n-form-item label="密码">
+                    <n-input v-model:value="info.password" placeholder="密码" type="password"></n-input>
+                </n-form-item>
+
+                <AgreeRule v-model="agreeRule" />
+                <n-button class="w-full text-base font-bold mt-4" @click="submit" type="primary">快 速 登 录</n-button>
+                <div class="flex justify-between text-xs mt-4">
+                    <a href="">忘记密码？</a>
+                    <span><span>没有账号？直接</span><a class="ml-1" :href="withBase('/user/login')">注册</a></span>
+
+                </div>
+
+            </n-form>
         </FormLayout>
     </n-modal>
 </template>
